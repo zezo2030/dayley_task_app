77026ac5f1098859ef2b52a6d9920d051c41452e
zoz <zezosaad194@gmail.com>
2025-01-13 14:26:44 +0200

    التعديل علي تعديل التاسك

--- lib/views/add_habit/add_habite_screen.dart
+++ lib/views/add_habit/add_habite_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dayley_task_app/utils/color_palette.dart';
 import 'package:dayley_task_app/widgets/padding_extension.dart';
 import 'package:dayley_task_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,8 @@ class AddHabiteScreen extends StatefulWidget {
 
 class _AddHabiteScreenState extends State<AddHabiteScreen> {
   final controller = TextEditingController();
+  bool isRepeatable = false;
+   TimeOfDay? timeOfHabits;
 
   @override
   Widget build(BuildContext context) {
@@ -28,14 +31,188 @@ class _AddHabiteScreenState extends State<AddHabiteScreen> {
           ).paddingAll(16),
           SizedBox(height: 40),
           SizedBox(
-            height: 50,
+            height: 60,
             child: buildTextFormField(
               controller,
               "your habites",
               true,
             ),
           ).paddingSymmetric(horizontal: 16),
-          SizedBox(height: 20),
+          SizedBox(height: 40),
+          Container(
+            height: 60,
+            width: double.infinity,
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isRepeatable = true;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: isRepeatable
+                            ? kPrimaryColor
+                            : kPrimaryColor.withAlpha(140),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                        ),
+                      ),
+                      child: Center(
+                          child: Text(
+                        'Repeatable',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isRepeatable = false;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: isRepeatable
+                            ? kPrimaryColor.withAlpha(140)
+                            : kPrimaryColor,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Center(
+                          child: Text(
+                        'Non Repeatable',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ).paddingSymmetric(horizontal: 16),
+          SizedBox(height: 40),
+          Container(
+            height: 50,
+            width: double.infinity,
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () async {
+                      TimeOfDay? pickedTime = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                      );
+                      if (pickedTime != null) {
+                        setState(() {
+                          // Update the time here
+                          timeOfHabits = pickedTime;
+                          print(timeOfHabits!.format(context));
+                        });
+                      }
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Pick Time',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 120,
+                  child: Text(
+                    timeOfHabits != null
+                        ? timeOfHabits!.format(context)
+                        : '00:00 AM',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ).paddingSymmetric(horizontal: 16),
+          SizedBox(height: 40),
+          Container(
+            height: 60,
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: ()=> Navigator.pop(context),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 30),
+                Expanded(
+                  child: GestureDetector(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Save',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ).paddingSymmetric(horizontal: 16),
         ],
       ),
     );
